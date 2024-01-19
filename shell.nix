1c3c7cddc09bb1fe90f958fa9e372a26c3713558
Derek Tracy <tracyde@gmail.com>
2024-01-19 18:18:17 -0500

    added direnv env setup to edit our configuration

---
+++ shell.nix
@@ -0,0 +1,10 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = with pkgs.buildPackages; [ 
+      sops 
+      age
+      ssh-to-age
+    ];
+}
+
