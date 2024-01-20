9129aa3168cd4cf8f296e310c7892f5266762662
Derek Tracy <tracyde@gmail.com>
2024-01-19 20:16:23 -0500

    trying very minimal config

--- users/testUser.nix
+++ users/testUser.nix
@@ -1,20 +1,20 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, home-manager, ... }: {
 
   programs.home-manager.enable = true;
 
   users.users.testUser.isNormalUser = true;
 
-  home-manager.users.testUser = { ... }: {
+  #home-manager.users.testUser = { ... }: {
 
-    home.username = "testUser";
-    home.homeDirectory = "/home/testUser";
+  #  home.username = "testUser";
+  #  home.homeDirectory = "/home/testUser";
 
-    home.packages = with pkgs; [
-      fish
-    ];
-    home.stateVersion = "23.05";
+  #  home.packages = with pkgs; [
+  #    fish
+  #  ];
+  #  home.stateVersion = "23.05";
 
-  };
+  #};
 
 
 }
