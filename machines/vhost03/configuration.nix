7445a2f3ea947a400060931233872da30e69eb26
Derek Tracy <tracyde@gmail.com>
2024-11-11 20:34:50 -0500

    removed quotes from package name

--- machines/vhost03/configuration.nix
+++ machines/vhost03/configuration.nix
@@ -35,7 +35,7 @@
 
   virtualisation.libvirtd.enable = true;
   virtualisation.libvirtd.allowedBridges = [ "br-mgmt" ];
-  environment.systemPackages = with pkgs; [ "virt-manager" ];
+  environment.systemPackages = with pkgs; [ virt-manager ];
 
   # Network configuration
   networking.useDHCP = false;
